2f5f2cc0ae1221c090f959ef8fd65e5c90dd6fe2
bhj1101 <9247bhj@gmail.com>
2022-04-13 17:38:57 +0900

    6주차

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,21 @@
+﻿class MainApp
+{
+    static void onWrongPathType(string type)
+    {
+        Console.WriteLine($"{type} is wrong type");
+        return;
+    }
+
+    static void Main(string[] args)
+    {
+        if(args.Length == 0)
+        {
+            Console.WriteLine(
+                "Usage : Touch.exe<Path> [Type:File/Directory]");
+            return;
+        }
+
+        string path = args[0];
+        string type = "File";
+    }
+}
